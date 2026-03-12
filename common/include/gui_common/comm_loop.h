9d4c144392f1a0a19fdb96969bdf16c468017722
Dawid Śliwa <dawid.sliwa@weles.dev>
2026-03-12 23:16:14 +0100

    feat: add CommLoop for decoupled communication
    Command queue that runs ConnectionViewModel::update() and processes
    connect/disconnect/sendConfig commands from any thread. Platform
    main loops drive it via runOnce() on a dedicated comm thread or task.

---
+++ common/include/gui_common/comm_loop.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "gui_common/view_models/connection_view_model.h"
+
+#include <atomic>
+#include <mutex>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace gui::common
+{
+
+class CommLoop
+{
+public:
+    struct ConnectCmd
+    {
+        std::string portPath;
+    };
+
+    struct DisconnectCmd
+    {
+    };
+
+    struct SendConfigCmd
+    {
+        midi2pwm::pwm::ChannelConfigT config;
+    };
+
+    struct RefreshPortsCmd
+    {
+    };
+
+    using Command = std::variant<ConnectCmd, DisconnectCmd, SendConfigCmd, RefreshPortsCmd>;
+
+    explicit CommLoop(ConnectionViewModel &vm);
+
+    void post(Command cmd);
+    void runOnce();
+    void requestStop();
+    bool shouldStop() const;
+
+private:
+    ConnectionViewModel &vm_;
+    std::mutex cmdMutex_;
+    std::vector<Command> pending_;
+    std::atomic<bool> stopRequested_{false};
+};
+
+} // namespace gui::common
---
+++ common/src/comm_loop.cpp
@@ -0,0 +1,63 @@
+#include "gui_common/comm_loop.h"
+#include "gui_common/log.h"
+
+namespace gui::common
+{
+
+static constexpr const char *TAG = "CommLoop";
+
+CommLoop::CommLoop(ConnectionViewModel &vm)
+    : vm_(vm)
+{
+}
+
+void CommLoop::post(Command cmd)
+{
+    std::lock_guard<std::mutex> lock(cmdMutex_);
+    pending_.push_back(std::move(cmd));
+}
+
+void CommLoop::runOnce()
+{
+    std::vector<Command> commands;
+    {
+        std::lock_guard<std::mutex> lock(cmdMutex_);
+        commands.swap(pending_);
+    }
+
+    for (auto &cmd : commands) {
+        std::visit([this](auto &c) {
+            using T = std::decay_t<decltype(c)>;
+
+            if constexpr (std::is_same_v<T, ConnectCmd>) {
+                GUI_LOG_INFO(TAG, "Connecting to %s", c.portPath.c_str());
+                if (!vm_.connect(c.portPath)) {
+                    GUI_LOG_ERROR(TAG, "Failed to connect to %s", c.portPath.c_str());
+                }
+            } else if constexpr (std::is_same_v<T, DisconnectCmd>) {
+                GUI_LOG_INFO(TAG, "Disconnecting");
+                vm_.disconnect();
+            } else if constexpr (std::is_same_v<T, SendConfigCmd>) {
+                if (!vm_.sendChannelConfig(c.config)) {
+                    GUI_LOG_ERROR(TAG, "Failed to send channel config");
+                }
+            } else if constexpr (std::is_same_v<T, RefreshPortsCmd>) {
+                vm_.refreshPorts();
+            }
+        }, cmd);
+    }
+
+    vm_.update();
+}
+
+void CommLoop::requestStop()
+{
+    stopRequested_.store(true, std::memory_order_release);
+}
+
+bool CommLoop::shouldStop() const
+{
+    return stopRequested_.load(std::memory_order_acquire);
+}
+
+} // namespace gui::common
